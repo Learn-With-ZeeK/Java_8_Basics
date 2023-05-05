93ae0e291d02f1f0a20b2aaac6722f10d3a3cddc
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2023-05-05 22:45:18 +0530

    Code for Predicate Interface- { filter condition internally uses }

--- src/com/java/pre/functional/demo/PredicateDemo.java
+++ src/com/java/pre/functional/demo/PredicateDemo.java
@@ -9,7 +9,7 @@ public class PredicateDemo {
 
 		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
 
-		             //Providing conditional check by using filter in stream(Extension of predicate demo)
+		              //Providing conditional check by using filter in stream(Extension of predicate demo)
 		list1.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("print  Even: " + t));
 	}
 
