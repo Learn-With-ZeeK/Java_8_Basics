71c95697f65e38132ce19e9374cb843905bcc361
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2023-05-05 22:50:19 +0530

    Database for Employee Class

--- src/com/java/stream/api/example/DataBase.java
+++ src/com/java/stream/api/example/DataBase.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class DataBase {
 
-	public static List<Employee> getEmployees() {
+	 public static List<Employee> getEmployees() {
 		List<Employee> list = new ArrayList<>();
 		list.add(new Employee(176, "Roshan", "IT", 600000));
 		list.add(new Employee(388, "Bikash", "CIVIL", 900000));
