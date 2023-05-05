c69d0551f359e3b14bbb6067568e3eec7933c583
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2023-05-05 22:39:57 +0530

    DataBase for MapVsFlatMap

--- src/com/java/map/EkartDataBase.java
+++ src/com/java/map/EkartDataBase.java
@@ -13,7 +13,7 @@ public class EkartDataBase {
 	                new Customer(102, "smith", "smith@gmail.com", Arrays.asList("89563865", "2487238947")),
 	                new Customer(103, "peter", "peter@gmail.com", Arrays.asList("38946328654", "3286487236")),
 	                new Customer(104, "kely", "kely@gmail.com", Arrays.asList("389246829364", "948609467"))
-	        ).collect(Collectors.toList());
+	         ).collect(Collectors.toList());
 	    }
 
 
