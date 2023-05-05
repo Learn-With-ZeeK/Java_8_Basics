de7308d48ae5e76b9a7b0e6f7e615c617122d42c
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2023-05-05 22:38:03 +0530

    Sorting or Organizing by ascending order of name

--- src/com/java/lamda/example/BookService.java
+++ src/com/java/lamda/example/BookService.java
@@ -12,7 +12,7 @@ public class BookService {
 		return books;
 	}
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		System.out.println(new BookService().getBooksInSortByName());
 	 }
 }
